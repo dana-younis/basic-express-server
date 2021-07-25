bcb727c161ae142c7c9723211c6b93f217ffa8f4
dana-younis <younisdana7@gmail.com>
2021-07-25 13:08:05 +0300

    server

--- index.js
+++ index.js
@@ -3,9 +3,3 @@ require('dotenv').config();
 const server = require('./src/server');
 
 server.start(process.env.PORT || 3000);
-app.get('/', (req, res) => {
-    res.send('I have deployed this using AWS Elastic Beanstalk');
-  });
-  app.get('/data', (req, res) => {
-    res.json({ a: 1, b: 2 });
-  });
